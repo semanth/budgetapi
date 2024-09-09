5fef7efeea0db1d6f70e23292571cd96a3d39a98
Naga-Semanth Munukutla <semanth@gmail.com>
2024-09-08 19:40:15 -0600

    Initial commit

---
+++ src/main/java/com/munukutla/budgetapi/BudgetBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.munukutla.budgetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/munukutla/budgetapi/BudgetBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munukutla.budgetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
